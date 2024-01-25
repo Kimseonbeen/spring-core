a6ade732127a6611e310c516a04c49afa494565a
Kimseonbeen <ksb9242@gmail.com>
2024-01-25 22:08:04 +0900

    생성자 주입을 선택하라 !

---
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -0,0 +1,38 @@
+package hello.core.autowired;
+
+import hello.core.member.Member;
+import jakarta.annotation.Nullable;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Optional;
+
+public class AutowiredTest {
+
+    @Test
+    void AutowiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    static class TestBean {
+
+        @Autowired(required = false)        // 의존 관계가 없을 시 수정자 메서드 자체가 호출이 안됌 !
+        public void setNoBean1(Member noBean1) {    // Member는 스프링 빈에 등록되어 있지 않음 !
+            System.out.println("noBean1 = " + noBean1);
+        }
+
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2) {
+            System.out.println("noBean2 = " + noBean2);
+        }
+
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3) {
+            System.out.println("noBean3 = " + noBean3);
+        }
+
+    }
+
+}
